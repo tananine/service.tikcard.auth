b445c74af5c043c9a6e2c5f89789ef96dc647d5a
tananine <tana9dev@gmail.com>
2023-04-10 14:04:50 +0700

    add env and refactor code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const rateLimit = require('express-rate-limit');
@@ -25,4 +27,7 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
-app.listen(3001);
+const PORT = process.env.PORT;
+const listener = app.listen(PORT, () =>
+  console.log('Server started on port ' + listener.address().port)
+);
--- functions/createRedis.js
+++ functions/createRedis.js
@@ -2,7 +2,8 @@ const redis = require('redis');
 
 exports.client = redis.createClient({
   socket: {
-    host: 'docker.for.mac.localhost',
-    port: '6379',
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
   },
+  password: process.env.REDIS_PASSWORD,
 });
