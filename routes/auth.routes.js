ae707c17fb878553ae1569ca4c8acc840ae04611
tananine <tana9dev@gmail.com>
2023-05-04 00:47:17 +0700

    add message to error validate

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,17 +7,29 @@ const router = express.Router();
 
 router.post(
   '/register',
-  [body('email').isEmail(), body('password').isLength({ min: 6, max: 15 })],
+  [
+    body('email').isEmail().withMessage('รูปแบบอีเมลไม่ถูกต้อง'),
+    body('password')
+      .isLength({ min: 6 })
+      .withMessage('รหัสผ่านต้องไม่น้อยกว่า 6 อักขระ'),
+  ],
   authControllers.register
 );
 router.post(
   '/login',
-  [body('email').isEmail(), body('password').isLength({ min: 6, max: 15 })],
+  [
+    body('email').isEmail().withMessage('รูปแบบอีเมลไม่ถูกต้อง'),
+    body('password')
+      .isLength({ min: 6 })
+      .withMessage('รหัสผ่านต้องไม่น้อยกว่า 6 อักขระ'),
+  ],
   authControllers.login
 );
 router.get(
   '/authentication',
-  header('Authorization').notEmpty(),
+  header('Authorization')
+    .notEmpty()
+    .withMessage('ไม่มี Authorization ใน Header'),
   authControllers.authentication
 );
 
