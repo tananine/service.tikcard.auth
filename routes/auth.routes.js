24391e5a77f2934904c3e14a6a23a710d003c2d2
tananine <tana9dev@gmail.com>
2023-07-31 23:13:33 +0700

    change validate auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,20 +8,34 @@ const router = express.Router();
 router.post(
   '/register',
   [
-    body('email').isEmail().withMessage('รูปแบบอีเมลไม่ถูกต้อง'),
+    body('email')
+      .trim()
+      .toLowerCase()
+      .isEmail()
+      .withMessage('รูปแบบอีเมลไม่ถูกต้อง')
+      .isLength({ max: 80 })
+      .withMessage('อีเมลต้องไม่เกิน 80 อักขระ'),
     body('password')
-      .isLength({ min: 6 })
-      .withMessage('รหัสผ่านต้องไม่น้อยกว่า 6 อักขระ'),
+      .trim()
+      .isLength({ min: 6, max: 28 })
+      .withMessage('รหัสผ่านต้องมี 6 ถึง 28 อักขระ'),
   ],
   authControllers.register
 );
 router.post(
   '/login',
   [
-    body('email').isEmail().withMessage('รูปแบบอีเมลไม่ถูกต้อง'),
+    body('email')
+      .trim()
+      .toLowerCase()
+      .isEmail()
+      .withMessage('รูปแบบอีเมลไม่ถูกต้อง')
+      .isLength({ max: 80 })
+      .withMessage('อีเมลต้องไม่เกิน 80 อักขระ'),
     body('password')
-      .isLength({ min: 6 })
-      .withMessage('รหัสผ่านต้องไม่น้อยกว่า 6 อักขระ'),
+      .trim()
+      .isLength({ min: 6, max: 28 })
+      .withMessage('รหัสผ่านต้องมี 6 ถึง 28 อักขระ'),
   ],
   authControllers.login
 );
